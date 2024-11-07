c4649f450e23109af8dd0291b839c1a8441a5232
Christian Ekenstedt <christian.ekenstedt@zero8.io>
2024-11-07 22:56:51 +0100

    fixes

--- test/zeatmap_test.dart
+++ test/zeatmap_test.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:zeatmap/zeat_map_legend_item.dart';
 
-import 'package:zeatmap/zeatmap.dart';
 import 'package:zeatmap/zeatmap_item.dart';
 import 'package:zeatmap/zeatmap_position.dart';
 
 void main() {
   test('ZeatMapItem', () {
     final position = ZeatMapPosition(0, 0);
-    final data = 'Row Data';
-    final color = Colors.red;
+    const data = 'Row Data';
+    const color = Colors.red;
     final date = DateTime.now();
-    final tooltipWidget = Text('Tooltip Widget');
+    const tooltipWidget = Text('Tooltip Widget');
     final zeatMapItem = ZeatMapItem(position,
         rowData: data, color: color, date: date, tooltipWidget: tooltipWidget);
 
@@ -24,8 +23,8 @@ void main() {
   });
 
   test('ZeatMapLegendItem', () {
-    final color = Colors.red;
-    final label = 'Label';
+    const color = Colors.red;
+    const label = 'Label';
     final zeatMapLegendItem = ZeatMapLegendItem(color, label);
 
     expect(zeatMapLegendItem.color, color);
